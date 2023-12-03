4ab9f33080616a9629edc4f06ab18559e73afa06
Maria Avanova <mariaavanova@192.168.1.3>
2023-12-03 19:41:20 +0300

    packaje

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React, { StrictMode } from 'react';
-import './index.css';
-import { createRoot } from 'react-dom/client';
-import App from './components/app';
-import { BrowserRouter } from 'react-router-dom';
+import React, { StrictMode } from "react";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./components/app";
+import { BrowserRouter } from "react-router-dom";
 
-const domNode = document.getElementById('root') as HTMLDivElement;
+const domNode = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(domNode);
 
 root.render(
-	<StrictMode>
-		<BrowserRouter>
-		<App />
-		</BrowserRouter>
-	</StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
